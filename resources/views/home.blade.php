a6ebf22e74cf9563f734b53804142113cb7034a1
itnomichi <38125513+itnomichi@users.noreply.github.com>
2018-06-16 10:54:46 +0700

    project creation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.main')
 @section('title','Trang chủ')
 @section('sidebar')
+    @section('search')
+        @include('home_search')
+    @endsection
     @include('layouts/sidebar')
 @endsection
 @section('content')
---
+++ resources/views/home_search.blade.php
@@ -0,0 +1,34 @@
+<div class="gb_Sd">
+    <form class="gb_Qe gb_of" method="get" role="search">
+        <h2 class="a3I">Tìm kiếm</h2>
+        <button class="gb_mf gb_lf" type="button">
+            <svg focusable="false" height="24px" viewBox="0 0 24 24" width="24px" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
+            </svg>
+        </button>
+        <div class="gb_rf gb_lf">
+            <input class="gb_9e" aria-label="Tìm kiếm" autocomplete="off"
+                   placeholder="Tìm kiếm" value="" name="q" type="text" dir="ltr"
+                   spellcheck="false" aria-haspopup="true" aria-live="off" aria-owns="gs_sbt50">
+        </div>
+        <button class="gb_qf" type="button" gh="sda" role="button">
+            <svg focusable="false" height="24px" viewBox="0 0 24 24" width="24px" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 10l5 5 5-5z"></path>
+                <path d="M0 0h24v24H0z" fill="none"></path>
+            </svg>
+        </button>
+        <button class="gb_nf" aria-label="Xóa tìm kiếm" type="button">
+            <svg focusable="false" height="24px" viewBox="0 0 24 24" width="24px" xmlns="http://www.w3.org/2000/svg">
+                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+                <path d="M0 0h24v24H0z" fill="none"></path>
+            </svg>
+        </button>
+        <button class="gb_bf gb_lf" aria-label="Tìm kiếm" role="button">
+            <svg focusable="false" height="24px" viewBox="0 0 24 24" width="24px" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                <path d="M0 0h24v24H0z" fill="none"></path>
+            </svg>
+        </button>
+    </form>
+</div>
